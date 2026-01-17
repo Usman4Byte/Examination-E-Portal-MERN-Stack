f6ad3aab35dbd589fc3badfa321386a81504ee1d
Muhammad Usman <usmanchaudhary112579@gmail.com>
2026-01-17 22:48:45 +0500

    Create index.js

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import studentRoutes from "./routes/StudentRoutes.js";
+import teacherRoutes from "./routes/teacherRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+
+/* MIDDLEWARE */
+app.use(cors());              // Allow frontend to talk to backend
+app.use(express.json());      // Allow JSON data in requests
+
+/* DATABASE */
+connectDB();
+
+/* ROUTES */
+app.use("/api/auth", authRoutes);
+app.use("/api/student", studentRoutes);
+app.use("/api/teacher", teacherRoutes);
+app.use('/api/categories', categoryRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
