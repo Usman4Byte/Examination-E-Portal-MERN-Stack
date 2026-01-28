481078042d906c2d3f0f506f2d57da45047feca0
Muhammad Usman <usmanchaudhary112579@gmail.com>
2026-01-28 15:02:15 +0500

    Create role.js

---
+++ server/src/middleware/role.js
@@ -0,0 +1,9 @@
+export default function role(allowedRoles = []) {
+    return (req, res, next) => {
+        if (!req.user) return res.status(401).json({ message: 'Not authenticated' });
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ message: 'Access denied' });
+        }
+        next();
+    };
+}
